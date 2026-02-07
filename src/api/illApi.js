ff73988c933cb24b9738d0348b1d6361ac05a57b
Wade Keith <wadekeith@Wades-MacBook-Air.local>
2026-02-07 17:10:43 -0600

    Fix: revert fetching back to original state

--- src/api/illApi.js
+++ src/api/illApi.js
@@ -1,8 +1,11 @@
 const BASE_URL = "https://api.impossiblelevels.com/api";
-const CORS_PROXY = "https://api.allorigins.win/raw?url=";
 
 export const getLevels = async () => {
-  const response = await fetch(`${CORS_PROXY}${BASE_URL}/levels`);
-  if (!response.ok) throw new Error("Failed to fetch levels");
+  const response = await fetch(`${BASE_URL}/levels`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch levels");
+  }
+
   return response.json();
 };
